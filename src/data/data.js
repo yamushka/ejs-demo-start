9bd2b4014aee71dacb4a5803e18005c4ae828f31
Frederick Roegiers <frederick@rogerthat.be>
2025-02-14 09:16:54 +0100

    add initial data structure for person, dinosaurs, and navigation items

---
+++ src/data/data.js
@@ -0,0 +1,97 @@
+export const person = {
+  firstname: "Frederick",
+  lastname: "Roegiers",
+  location: {
+    city: "Ghent",
+    country: "Belgium",
+  },
+};
+
+export const dinosaurs = [
+  {
+    id: 1,
+    isHerbivore: true,
+    name: "Edaphosaurus",
+    slug: "edaphosaurus",
+    image: "dino_01.png",
+    description:
+      "The Edaphosaurus is a herbivorous dinosaur that lived during the Carboniferous period.",
+  },
+  {
+    id: 2,
+    name: "Pterodactyl",
+    isHerbivore: false,
+    slug: "pterodactyl",
+    image: "dino_02.png",
+    description:
+      "The Pterodactyl is a flying reptile from the time of the dinosaurs. It lived during the Late Jurassic period.",
+  },
+  {
+    id: 3,
+    name: "Xenoceratops",
+    isHerbivore: true,
+    slug: "xenoceratops",
+    image: "dino_03.png",
+    description:
+      "The Xenoceratops is a herbivorous dinosaur that lived during the Late Cretaceous period.",
+  },
+  {
+    id: 4,
+    name: "Spinosaurus",
+    isHerbivore: false,
+    slug: "spinosaurus",
+    image: "dino_04.png",
+    description:
+      "The Spinosaurus is a large, carnivorous dinosaur. It lived during the Late Cretaceous period.",
+  },
+  {
+    id: 5,
+    name: "Diplodocus",
+    isHerbivore: true,
+    slug: "diplodocus",
+    image: "dino_05.png",
+    description:
+      "The Diplodocus is a large, long-necked, herbivorous dinosaur. It lived during the Late Jurassic period.",
+  },
+  {
+    id: 6,
+    isHerbivore: true,
+    name: "Parasaur",
+    slug: "parasaur",
+    image: "dino_06.png",
+    description:
+      "The Parasaur is a herbivorous dinosaur with a distinctive crest on its head. It lived during the Late Cretaceous period.",
+  },
+  {
+    id: 7,
+    isHerbivore: true,
+    name: "Brontosaurus",
+    slug: "brontosaurus",
+    image: "dino_07.png",
+    description:
+      "The Brontosaurus is a large, herbivorous dinosaur. It lived during the Late Jurassic period.",
+  },
+  {
+    id: 8,
+    name: "Stegosaurus",
+    slug: "stegosaurus",
+    image: "dino_08.png",
+    description:
+      "The Stegosaurus is a large, plant-eating dinosaur. It has a distinctive row of large, bony plates that extend along its back and a long, spiky tail.",
+  },
+];
+
+export const navItems = [
+  {
+    url: "/",
+    label: "Home",
+  },
+  {
+    url: "/dinosaurs",
+    label: "Dinosaurs",
+  },
+  {
+    url: "/contact",
+    label: "Contact",
+  },
+];
