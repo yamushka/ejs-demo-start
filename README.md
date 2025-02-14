f622d85e9fc0accfc6cb31edbd47d7a47827c333
Frederick Roegiers <frederick@rogerthat.be>
2025-02-14 09:07:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Templating Engines met EJS - END
+
+## Description
+This project is part of the course Programming 3 at Arteveldehogeschool Gent. This is the start code for the livecode demo we'll develop during class.
+
+
+## Installation
+
+```bash
+$ npm install
+```
+
+**Note:** normally we **do not** include a .env file, ONLY because this is a demo we included the file.
+
+## Running the App
+
+```bash
+# watch mode
+$ npm run start:dev
+```
+
+## Useful references
+- [EJS Documentation](https://ejs.co/#docs)
+- [Express EJS Layouts](https://www.npmjs.com/package/express-ejs-layouts)
+- [Marked Documentation](https://marked.js.org/)
+
+## Authors
+- Frederick Roegiers
---
+++ src/app.js
@@ -0,0 +1,23 @@
+// import statements
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+// create an instance of express
+const app = express();
+
+// serve static files from the public folder
+// they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
+app.use(express.static("public"));
+
+// GET route to serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("src", "views", "index.html"));
+});
+
+// start the server, listen on port defined in .env file
+app.listen(process.env.PORT, () => {
+  // callback function that is called when the server starts
+  console.log(`Application is listening to port ${process.env.PORT}.`);
+});
