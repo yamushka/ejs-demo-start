9da07a4226611801ba7bf47245533db578c8b1b6
yamushka <joanjode@student.arteveldehs.be>
2025-02-18 09:34:04 +0100

    changes to template engine

--- src/app.js
+++ src/app.js
@@ -5,14 +5,22 @@ import path from "path";
 // create an instance of express
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "awesome",
+    content: "dino's are awesome"
+  })
 });
+
+
 
 // start the server, listen on port defined in .env file
 app.listen(process.env.PORT, () => {
